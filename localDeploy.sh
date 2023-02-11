2ce39ab71f4f6e91cbac2d2e62c9d56bd87cec69
Jasen Carroll <11709645+jasenc@users.noreply.github.com>
2023-02-10 23:34:59 -0800

    scripts to up local deploy

---
+++ localDeploy.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+## ENSURE THAT YOU KNOW YOUR locahost:PORT NUMBER PRIOR TO RUNNING SCRIPT ##
+## AND HAVE SAVED ACCORDINGLY IN YOUR settings.py ##
+
+# move into the API
+cd NeighborlyAPI
+# install dependencies
+pipenv install
+# go into the shell
+pipenv shell
+#test functionApp locally 
+func start
+
+# move over to the client-side
+cd ../NeighborlyFrontEnd
+
+# install frontend dependencies
+pipenv install
+
+# go into shell
+pipenv shell
+
+# test the webapp locally
+python app.py
+
